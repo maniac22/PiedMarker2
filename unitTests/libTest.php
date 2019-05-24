86d242c0340a018a13311b99c852a68af129a570
maniac22 <mphomathabatha15@gmail.com>
2019-05-24 14:13:28 -0400

    Update libTest.php

--- unitTests/libTest.php
+++ unitTests/libTest.php
@@ -1,7 +1,6 @@
 <?php
 use PHPUnit\Framework\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
-require_once('/home/travis/build/maniac22/PiedMarker2/marker2/app_prototypes/libPrototype.php');
 class libTest extends TestCase{
     public function test_Test(){
         $tester=Test();
