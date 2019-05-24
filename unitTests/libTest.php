b2754c4168389c12979b15cda9ea4a5c8c07667d
maniac22 <mphomathabatha15@gmail.com>
2019-05-24 11:38:05 -0400

    Update libTest.php

--- unitTests/libTest.php
+++ unitTests/libTest.php
@@ -1,7 +1,7 @@
 <?php
 use PHPUnit\Framework\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
-require_once('libPrototype.php');
+require_once('../libPrototype.php');
 class libTest extends TestCase{
     public function test_Test(){
         $tester=Test();
