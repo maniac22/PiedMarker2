62caadce65ba9f375eb84cfc1e01dc10fc41cdbd
maniac22 <mphomathabatha15@gmail.com>
2019-05-24 19:49:57 +0000

    lets see

--- unitTests/libTest.php
+++ unitTests/libTest.php
@@ -4,8 +4,9 @@ use PHPUnit\DbUnit\TestCaseTrait;
 class libTest extends TestCase{
     public function test_Test_2(){
         $temp=new mork;
-        $tester=temp->Test();
+        $tester=$temp->Test();
         $result = $tester;
+        $expected = array("Classic Mode", "Fastest Mode", "Tournament Mode", "AI Mode");
         $this->assertEquals(2,$result,"correct!");
     }
  
