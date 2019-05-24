c95512abcaf45229d46f46b74683acb964a02010
maniac22 <mphomathabatha15@gmail.com>
2019-05-24 20:30:01 +0000

    designed

--- unitTests/libTest.php
+++ unitTests/libTest.php
@@ -3,7 +3,6 @@ use PHPUnit\Framework\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
 class lib extends TestCase{
     public function test_Test_2(){
-        $it=new tuna;
         $expected = array("Classic Mode", "Fastest Mode", "Tournament Mode", "AI Mode");
         $this->assertEquals(2,2,"correct!");
     }
