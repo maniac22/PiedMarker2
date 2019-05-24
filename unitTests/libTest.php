e5ebccbe408973c4c5f0782734df1a22fdb4222b
maniac22 <mphomathabatha15@gmail.com>
2019-05-24 14:18:07 -0400

    Update libTest.php

--- unitTests/libTest.php
+++ unitTests/libTest.php
@@ -2,7 +2,7 @@
 use PHPUnit\Framework\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
 class libTest extends TestCase{
-    public function test_Test(){
+    public function test_Test_2(){
         $tester=Test();
         $result = $tester;
         $expected = array("Classic Mode", "Fastest Mode", "Tournament Mode", "AI Mode");
