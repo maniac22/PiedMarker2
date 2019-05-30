f1458845b7a7e5939610980db01f5493cd8b40dc
maniac22 <mphomathabatha15@gmail.com>
2019-05-30 23:55:49 +0000

    d

--- unitTests/libTest.php
+++ unitTests/libTest.php
@@ -3,9 +3,9 @@ use PHPUnit\Framework\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
 class lib extends TestCase{
     public function test_test(){
-    	require("nan.php");
+    	require("/home/travis/build/maniac22/PiedMarker2/app_prototypes/test.php");
         $expected = array("Classic Mode", "Fastest Mode", "Tournament Mode", "AI Mode");
-       $this->expectOutputString('3');
+       $this->expectOutputString('40');
     }
  
  
